0e4d1054923d59ba1b56ebd80565b400204559a2
Josh Kaurich <joshkaurichpro@gmail.com>
2019-11-16 18:27:31 -0500

    Add files via upload

---
+++ AbstractSingleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public abstract class AbstractSingleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+  // Singleton instance
+  private static T instance;
+
+  // Singleton instance accessor
+  public static T Instance { get { return instance; } }
+
+  // Awake is called when the script instance is being loaded
+  protected virtual void Awake() {
+    if ( instance != null ) {
+      Destroy(gameObject);
+      return;
+    } else {
+      instance = this as T;
+    }
+  }
+}
