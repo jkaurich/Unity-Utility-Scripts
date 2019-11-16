109914e951c3adf0f86fddcfe7cf9a9c6a255b57
Josh Kaurich <joshkaurichpro@gmail.com>
2019-11-15 20:15:26 -0500

    Generic Singleton MonoBehaviour
    Inheritable monobehaviour singleton

---
+++ Singleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+  protected static T instance;
+
+  public static T Instance { get { return instance; } }
+
+  protected virtual void CheckExistance() {
+    if ( instance != null ) {
+      Destroy(gameObject);
+      return;
+    } else {
+      instance = this as T;
+    }
+  }
+
+  protected virtual void Awake() {
+    CheckExistance();
+  }
+}
